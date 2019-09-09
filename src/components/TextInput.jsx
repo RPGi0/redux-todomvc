0704c4a1d4638d31f243e1d6f5e1952145dc096a
Rob <gio@llnnll.com>
2019-09-09 17:19:09 -0400

    Updated TextInput, TodoApp; Created Footer

--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
+import classnames from 'classnames';
 
 export default class TextInput extends React.Component {
   constructor(props) {
     super(props);
+    this.state={value: props.text};
     this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+  }
+
+  cancelEditing() {
+    this.setState({'value': this.props.text});
+    return this.props.cancelEditing(this.props.itemId);
+  }
+
+  _handleKeyDown(e) {
+    switch (e.key) {
+      case 'Enter':
+        return this.props.doneEditing(this.props.itemId, this.state.value);
+      case 'Escape':
+        return this.cancelEditing();
+    }
+  }
+
+  _handleOnBlur(e) {
+    return this.cancelEditing();
+  }
+
+  _handleOnChange(e) {
+    this.setState({value: e.target.event});
   }
 
   render() {
@@ -12,6 +37,11 @@ export default class TextInput extends React.Component {
       <input className="edit"
              autoFocus={true}
              type="text"
+             ref="itemInput"
+             value={this.state.value}
+             onChange={this._handleOnChange.bind(this)}
+             onKeyDown={this._handleKeyDown.bind(this)}
+             onBlur={this._handleOnBlur.bind(this)}
       />
     )
   }
--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -1,16 +1,31 @@
 import React from 'react';
 
 import TodoList from './TodoList';
+import TodoHeader from './TodoHeader';
+import TodoTools from './TodoTools';
+import Footer from './Footer';
 
 export default class TodoApp extends React.Component {
-  getItems() {
-    return this.props.todos || [];
+  getNbActiveItems() {
+    if (this.props.todos) {
+      const activeItems = this.props.todos.filter(
+        (item) => item.get('status') === 'active'
+      );
+      return activeItems.size;
+    }
+    return 0;
   }
+
   render() {
     return <div>
       <section id="todoapp">
+        <TodoHeader />
         <TodoList todos={this.props.todos} filter={this.props.filter}/>
+        <TodoTools filter={this.props.filter}
+                   nbActiveItems={this.getNbActiveItems()}
+        />
       </section>
+      <Footer />
     </div>
   }
 }
