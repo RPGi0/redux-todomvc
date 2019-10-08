6e3861978b71d4c4cf772725a30a46f8e15f049c
Rob <gio@llnnll.com>
2019-10-08 15:38:26 -0400

    added react-devtools

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -19,19 +19,23 @@ export default class TodoItem extends React.Component {
         <input type="checkbox"
                className="toggle"
                defaultChecked={this.props.isCompleted}
-               onClick={() => this.props.toggleComplete(this.props.id)} />
+               onClick={() => this.props.toggleComplete(this.props.id)} 
+        />
         <label htmlFor="todo"
                ref="text"
-               onDoubleClick={() => this.props.editItem(this.props.id)}>
+               onDoubleClick={() => this.props.editItem(this.props.id)}
+        >
           {this.props.text}
         </label>
         <button className="destroy"
-                onClick={() => this.props.deleteItem(this.props.id)}></button>
+                onClick={() => this.props.deleteItem(this.props.id)}>
+        </button>
       </div>
       <TextInput text={this.props.text}
                  itemId={this.props.id}
                  cancelEditing={this.props.cancelEditing}
-                 doneEditing={this.props.doneEditing} />
+                 doneEditing={this.props.doneEditing} 
+      />
     </li>
   }
 };
